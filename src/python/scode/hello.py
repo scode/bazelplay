b6ac4c10c940e1693a0a7a2fa35b320735fe46a4
Peter Schuller <peter.schuller@infidyne.com>
2018-03-24 01:41:19 -0700

    python 2 -> python 3

--- src/python/scode/hello.py
+++ src/python/scode/hello.py
@@ -1,5 +1,5 @@
-def main():
-    print 'Hello world!'
+def main() -> None:
+    print('Hello world!')
 
 if __name__ == '__main__':
     main()
--- src/python/scode/hello_test.py
+++ src/python/scode/hello_test.py
@@ -1,4 +1,4 @@
 from src.python.scode import hello
 
-def test_hello():
+def test_hello() -> None:
     hello.main()
